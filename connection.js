fd611521916f7f390cb7beb1d8f32f7fa86c59b7
maxedsdb <94120240+maxedsdb@users.noreply.github.com>
2021-12-06 15:02:12 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,12 +1,1 @@
-const mysql = require('mysql');
-const db = mysql.createConnection({
-  host: 'sql6.freemysqlhosting.net',
-  user: 'sql6447346',
-  password: '3EKGlxnRrM',
-  database:'sql6447346'
-});
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-module.exports=db;
+
